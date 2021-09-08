73872e4c3a3db5436852dcdb5651b9ba0fbc38fd
hurshh <quasarhrsh@gmail.com>
2021-09-08 10:31:13 +0530

    date1

--- codeChef/src/DATE1.java
+++ codeChef/src/DATE1.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class DATE1 {
+import java.util.Scanner;
+
+class DATE1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0){
+            long a = sc.nextLong();
+            long y = sc.nextLong(); //life of lit candle
+            long x = sc.nextLong(); //number of candles lit further
+            long ans;
+            if(a>=y)
+            {
+                ans = (long)x*y;
+                System.out.println(ans);
+            }
+            else
+            {
+                ans = (long)a*x+1;
+                System.out.println(ans);
+            }
+        }
+    }
 }
