d60f74fee8f028be05151f18571a525aac706691
hurshh <quasarhrsh@gmail.com>
2021-09-12 22:15:11 +0530

    ferris wheel

--- CSES/intro/src/ferrisWheel.java
+++ CSES/intro/src/ferrisWheel.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class ferrisWheel {
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class ferrisWheel {
+    public static void main(String[] args) throws IOException {
+        Reader rdr = new Reader();
+        int n = rdr.nextInt();
+        int x = rdr.nextInt();
+        long numberOfGondola =0;
+        int leftIndex =0;
+        int rightIndex = n-1;
+        Integer[] arr = new Integer[n];
+        for(int i=0;i<n;i++){
+            arr[i] = rdr.nextInt();
+        }
+        Arrays.sort(arr);
+        while(rightIndex>leftIndex){
+            if(arr[rightIndex]+arr[leftIndex]<=x ){
+                numberOfGondola++;
+                rightIndex--;
+                leftIndex++;
+            }
+            else{
+                numberOfGondola++;
+                rightIndex--;
+            }
+        }
+        if(leftIndex==rightIndex && arr[leftIndex]<x){
+            numberOfGondola++;
+        }
+        System.out.println(numberOfGondola);
+    }
 }
+
