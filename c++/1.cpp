2d57f1660063ea139637e3d455b17e84b9801036
hurshh <quasarhrsh@gmail.com>
2021-11-22 13:23:15 +0530

    22-11-2021

---
+++ c++/1.cpp
@@ -0,0 +1,39 @@
+//SLUMPGAWD
+using namespace std; 
+#include <bits/stdc++.h> 
+ 
+#define rep(i,n) for(auto i=0; i<(n); i++) 
+#define mem(x,val) memset((x),(val),sizeof(x)); 
+#define rite(x) freopen(x,"w",stdout); 
+#define read(x) freopen(x,"r",stdin); 
+#define all(x) x.begin(),x.end() 
+#define sz(x) ((int)x.size()) 
+#define sqr(x) ((x)*(x)) 
+#define pb push_back 
+#define clr clear() 
+#define inf (1<<30) 
+#define ins insert 
+#define xx first 
+#define yy second 
+#define eps 1e-9 
+ 
+typedef long long ll; 
+typedef unsigned long long ui64; 
+typedef string st; 
+typedef vector<int> vi; 
+typedef vector<st> vs; 
+typedef map<int,int> mii; 
+typedef map<st,int> msi; 
+typedef set<int> si; 
+typedef set<st> ss; 
+typedef pair<int,int> pii; 
+typedef vector<pii> vpii; 
+ 
+#define mx 0 
+ 
+int main() { 
+    ios_base::sync_with_stdio(0); 
+    cout<<"hello world\n";
+    cout<<2;
+    return 0; 
+} 
